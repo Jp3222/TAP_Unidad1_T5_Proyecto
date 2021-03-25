1c0a436f59fa794087a3a2ef4ff514c03fe8ef04
Juan Pablo Campos <64780248+Jp3222@users.noreply.github.com>
2021-03-24 22:52:07 -0600

    Create TAP_Unidad1_T5_P.java

---
+++ TAP_Unidad1_T5_P/tap_unidad1_t5_p/TAP_Unidad1_T5_P.java
@@ -0,0 +1,12 @@
+package tap_unidad1_t5_p;
+
+import Interfaz.*;
+import javax.swing.SwingUtilities;
+
+public class TAP_Unidad1_T5_P {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Ventana_C().setVisible(true));
+    }
+
+}
